901d67d96eec20e52213a3a34fe854f6455d119e
Temidayo Oladele <51400490+Temidayomichael@users.noreply.github.com>
2024-08-06 10:34:47 +0100

    Update globalConfig.js

--- utils/globalConfig.js
+++ utils/globalConfig.js
@@ -1,5 +1,5 @@
 
 
 export const BACKEND_URL = import.meta.env.VITE_DEV_ENV == "production"
-                    ? "https://belocated-app-api.onrender.com"
+                    ? "https://backend-dev-p804.onrender.com"
                     : "http://localhost:6001"
