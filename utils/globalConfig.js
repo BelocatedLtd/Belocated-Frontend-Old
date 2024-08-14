bbfa316de35ee15fa1d5c2fad19814e39e1604d9
Temidayo Oladele <51400490+Temidayomichael@users.noreply.github.com>
2024-08-14 09:34:05 +0100

    Update globalConfig.js

--- utils/globalConfig.js
+++ utils/globalConfig.js
@@ -1,5 +1,5 @@
 
 
 export const BACKEND_URL = import.meta.env.VITE_DEV_ENV == "production"
-                    ? "https://backend-dev-p804.onrender.com"
+                    ? "admin-belocated-backend.vercel.app"
                     : "http://localhost:6001"
