c1cd91c306131136276e56b6684010ac998c8660
Temidayo Oladele <51400490+Temidayomichael@users.noreply.github.com>
2024-08-26 00:57:39 +0100

    ...

--- utils/globalConfig.js
+++ utils/globalConfig.js
@@ -1,5 +1,6 @@
 
 
-export const BACKEND_URL = import.meta.env.VITE_DEV_ENV == "production"
-                    ? "https://admin-belocated-backend.vercel.app"
-                    : "http://localhost:6001"
+export const BACKEND_URL =
+	import.meta.env.VITE_DEV_ENV == 'production'
+		? 'https://belocated-backend-gamma.vercel.app/'
+		: 'http://localhost:6001'
