cb72b575140e70f4cf0062201af0daa4fd8d2646
Temidayo Oladele <belokated@gmail.com>
2024-10-02 19:24:13 +0100

    ...

--- utils/globalConfig.js
+++ utils/globalConfig.js
@@ -1,4 +1,4 @@
 export const BACKEND_URL =
-	import.meta.env.VITE_DEV_ENV == 'production'
+	import.meta.env.VITE_DEV_ENV === 'production'
 		? 'https://dolphin-app-esx2u.ondigitalocean.app'
 		: 'http://localhost:6001'
