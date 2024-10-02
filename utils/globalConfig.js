33341711bb204eab91e773ed1f49e8a638dcd60a
Temidayo Oladele <51400490+Temidayomichael@users.noreply.github.com>
2024-10-02 16:45:47 +0100

    prod backend link

--- utils/globalConfig.js
+++ utils/globalConfig.js
@@ -1,4 +1,4 @@
 export const BACKEND_URL =
 	import.meta.env.VITE_DEV_ENV == 'production'
-		? 'https://admin-belocated-backend-sooty.vercel.app'
+		? 'https://dolphin-app-esx2u.ondigitalocean.app'
 		: 'http://localhost:6001'
