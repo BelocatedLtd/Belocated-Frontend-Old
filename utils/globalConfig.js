5d707eeb185c7faa8ed19b099a8a1db2de4030ba
BelocatedLtd <belocatedltd@gmail.com>
2024-10-10 16:22:33 +0100

    Update globalConfig.js

--- utils/globalConfig.js
+++ utils/globalConfig.js
@@ -1,4 +1,1 @@
-export const BACKEND_URL =
-	import.meta.env.VITE_DEV_ENV === 'production'
-		? 'https://dolphin-app-esx2u.ondigitalocean.app'
-		: 'http://localhost:6001' 
+export const BACKEND_URL = 'https://dolphin-app-esx2u.ondigitalocean.app';
