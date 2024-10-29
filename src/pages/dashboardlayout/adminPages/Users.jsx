ef22ee5e9002ad3ea750635ac623dda545d1798d
BelocatedLtd <belocatedltd@gmail.com>
2024-10-29 10:09:00 +0100

    Update Users.jsx

--- src/pages/dashboardlayout/adminPages/Users.jsx
+++ src/pages/dashboardlayout/adminPages/Users.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import DataTable from 'react-data-table-component'
-import { toast } from 'react-hot-toast'
+import toast from 'react-hot-toast';
 import { AiFillDelete } from 'react-icons/ai'
 import { MdArrowDownward, MdOutlineKeyboardArrowLeft } from 'react-icons/md'
 import { useDispatch, useSelector } from 'react-redux'
