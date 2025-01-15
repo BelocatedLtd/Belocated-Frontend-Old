a456cb64a61abc9a332fabefc4da3fdbc58e9298
BelocatedLtd <belocatedltd@gmail.com>
2025-01-15 15:42:32 +0100

    Update userServices.js

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,34 +1,27 @@
-import axios from 'axios'
-import { BACKEND_URL } from '../../utils/globalConfig'
+import axios from 'axios';
+import { BACKEND_URL } from '../../utils/globalConfig';
 import { getToken } from '../../utils/tokenHandler'
 import toast from 'react-hot-toast';
 
-const getAuthHeaders = () => {
-	const token = getToken()
-
-	if (token) {
-		return {
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		}
-	}
-}
-
-//Get User
-export const getAllUser = async (page, limit, search, startDate,endDate) => {
-	const headers = getAuthHeaders()
+// Get User
+export const getAllUser = async (page, limit, search, startDate, endDate) => {
 	try {
+		const token = getToken(); // Assuming getToken() is defined somewhere
+		const headers = {
+			Authorization: `Bearer ${token}`,
+		};
+
 		const response = await axios.get(
 			`${BACKEND_URL}/api/user/all?page=${page}&limit=${limit}&search=${search || ''}&startDate=${startDate || ''}&endDate=${endDate || ''}`,
 			{ headers }
-		  );
-		return response.data
+		);
+
+		return response.data;
 	} catch (error) {
 		const message =
 			(error.response && error.response.data && error.response.data.message) ||
 			error.message ||
-			error.toString()
-		toast.error(message)
+			error.toString();
+		toast.error(message);
 	}
-}
+};
