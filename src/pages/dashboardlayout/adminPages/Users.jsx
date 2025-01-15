40da918fb7ec2528465128b93c3a6eb1050829b2
BelocatedLtd <belocatedltd@gmail.com>
2025-01-15 14:54:21 +0100

    Update Users.jsx analytics

--- src/pages/dashboardlayout/adminPages/Users.jsx
+++ src/pages/dashboardlayout/adminPages/Users.jsx
@@ -1,12 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import DataTable from 'react-data-table-component';
-import { MdOutlineKeyboardArrowLeft } from 'react-icons/md';
-import { PieChart, Pie, Cell, Tooltip } from 'recharts';
-import { useNavigate } from 'react-router-dom';
-import { getAllUser } from '../../../services/userServices';
+import React, { useEffect, useState } from 'react'
+import DataTable from 'react-data-table-component'
+import toast from 'react-hot-toast';
+import { AiFillDelete } from 'react-icons/ai'
+import { MdArrowDownward, MdOutlineKeyboardArrowLeft } from 'react-icons/md'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import DataSearch from '../../../components/adminComponents/DataSearch'
+import Loader from '../../../components/loader/Loader'
+import {
+	handleGetAllActivities,
+	selectActivities,
+} from '../../../redux/slices/feedSlice'
+import { selectTasks } from '../../../redux/slices/taskSlice'
+import { selectIsLoading } from '../../../redux/slices/userSlice'
+import { trashAllUserActivities } from '../../../services/feedService'
+import { getAllUser } from '../../../services/userServices'
 
 const Users = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch()
+	const tasks = useSelector(selectTasks)
+	const activities = useSelector(selectActivities)
+	const sortIcon = <MdArrowDownward />
+	const [activityIsLoading, setactivityIsLoading] = useState(false)
   const [currentPage, setCurrentPage] = useState(1);
   const [totalRows, setTotalRows] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
@@ -25,13 +41,99 @@ const Users = () => {
 
 	
   });
+  useEffect(() => {
+	dispatch(handleGetAllActivities())
+}, [])
 
+const trashAllActivities = async () => {
+	if (activities.length < 500) {
+		toast.error('Lets allow it get to atleast 500 activities in the feed')
+		return
+	}
+
+	setactivityIsLoading(true)
+	const response = trashAllUserActivities()
+		.catch((error) => {
+			setactivityIsLoading(false)
+			toast.error("Failed to trash users' activities")
+			console.log(error)
+		})
+		.then((res) => {
+			setactivityIsLoading(false)
+			console.log(res)
+		})
+
+	toast.promise(response, {
+		loading: 'Emptying activity feed...',
+		success: <b>Activity feed emptied successfully</b>,
+		error: <b>Failed to emptying activity feed</b>,
+	})
+}
+
+const handleFilter = (e) => {
+	e.preventDefault()
+	const newData = fetchUsers(currentPage, rowsPerPage, e?.target?.value)
+}
+const columns = [
+    { 
+		name: 'Fullname', 
+		selector: (row) => row.fullname 
+	},
+    { name: 'Username', selector: (row) => row.username, sortable: true },
+    { name: 'Email', selector: (row) => row.email },
+    { name: 'Phone', selector: (row) => row.phone, sortable: true },
+    { name: 'State', selector: (row) => row.location, sortable: true },
+    { name: 'Gender', selector: (row) => row.gender, sortable: true },
+    { name: 'Tasks Completed', selector: (row) => row.taskCompleted, sortable: true },
+	{
+		name: 'Referred Users',
+		selector: (row) => row.referrals?.length,
+		sortable: true,
+	},
+	{
+		name: 'Actions',
+		button: true,
+		cell: (row) => (
+			<button
+				className='bg-[#18141E] text-gray-100 px-3 py-2 rounded-2xl hover:bg-btn hover:bg-secondary'
+				onClick={(e) => handleButtonClick(e, row._id)}>
+				View User
+			</button>
+		),
+	},
+  ];
   const COLORS = ['#4bc0c0', '#ffce56', '#36a2eb'];
+  const customStyles = {
+	headCells: {
+		style: {
+			backgroundColor: '#18141E',
+			color: '#f4f4f4',
+			fontSize: '15px',
+		},
+	},
+}
 
-  const fetchUsers = async (page, limit,startDate, endDate) => {
+const handleButtonClick = (e, userId) => {
+	e.preventDefault()
+	navigate(`/admin/dashboard/user/${userId}`)
+}
+const handleStartDateChange = (e) => setStartDate(e.target.value);
+  const handleEndDateChange = (e) => setEndDate(e.target.value);
+  const applyDateFilter = () => fetchUsers(currentPage, rowsPerPage, startDate, endDate);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    fetchUsers(page, rowsPerPage, startDate, endDate);
+  };
+
+  const handleChangeRowsPerPage = (rowsPerPage) => {
+    setRowsPerPage(rowsPerPage);
+    fetchUsers(currentPage, rowsPerPage, startDate, endDate);
+  };
+  const fetchUsers = async (page, limit, search,startDate, endDate) => {
     try {
       setIsLoading(true);
-      const response = await getAllUser(page, limit, startDate, endDate);
+      const response = await getAllUser(page, limit, search, startDate, endDate);
       if (response) {
         setTotalRows(response.totalUsers);
         setUsers(response.users);
@@ -52,34 +154,13 @@ const Users = () => {
     }
   };
 
-  const handleStartDateChange = (e) => setStartDate(e.target.value);
-  const handleEndDateChange = (e) => setEndDate(e.target.value);
-  const applyDateFilter = () => fetchUsers(currentPage, rowsPerPage, startDate, endDate);
-
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-    fetchUsers(page, rowsPerPage, startDate, endDate);
-  };
-
-  const handleChangeRowsPerPage = (rowsPerPage) => {
-    setRowsPerPage(rowsPerPage);
-    fetchUsers(currentPage, rowsPerPage, startDate, endDate);
-  };
+  
 
   useEffect(() => {
     fetchUsers(currentPage, rowsPerPage, startDate, endDate);
   }, []);
 
-  const columns = [
-    { name: 'Fullname', selector: (row) => row.fullname },
-    { name: 'Username', selector: (row) => row.username, sortable: true },
-    { name: 'Email', selector: (row) => row.email },
-    { name: 'Phone', selector: (row) => row.phone, sortable: true },
-    { name: 'State', selector: (row) => row.location, sortable: true },
-    { name: 'Gender', selector: (row) => row.gender, sortable: true },
-    { name: 'Tasks Completed', selector: (row) => row.taskCompleted, sortable: true },
-    { name: 'Referrals', selector: (row) => row.referrals?.length, sortable: true },
-  ];
+ 
 
   const pieChartData = [
     { name: 'Tasks Completed', value: summary.totalTasksCompleted },
@@ -88,17 +169,28 @@ const Users = () => {
   ];
 
   return (
-    <div className="w-full mx-auto mt-6 p-4">
-      {/* Back Button */}
-      <div className="flex items-center mb-6">
-        <MdOutlineKeyboardArrowLeft
-          size={30}
-          onClick={() => navigate(-1)}
-          className="cursor-pointer mr-2"
-        />
-        <h1 className="text-xl font-bold text-gray-700">Users</h1>
-      </div>
+	<div className='w-full mx-auto mt-[2rem]'>
+	{activityIsLoading && <Loader />}
+	<div className='flex items-center justify-between mb-[2rem]'>
+		<div className='flex items-center'>
+			<MdOutlineKeyboardArrowLeft
+				size={30}
+				onClick={() => navigate(-1)}
+				className='mr-1'
+			/>
+			<p className='font-semibold text-xl text-gray-700'>Users</p>
+</div>
 
+<div className='flex items-center gap-2'>
+	<label>User Activities:</label>
+
+	<p>{activities.length}</p>
+					<AiFillDelete
+						className='text-secondary hover:text-tertiary'
+						onClick={trashAllActivities}
+					/>
+					</div>
+			</div>
       {/* Summary */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-6">
         <div className="p-4 bg-white shadow rounded">
@@ -175,16 +267,23 @@ const Users = () => {
 	  </div>
 
       {/* User Table */}
-      <DataTable
-        columns={columns}
-        data={users}
-        progressPending={isLoading}
-        pagination
-        paginationServer
-        paginationTotalRows={totalRows}
-        onChangePage={handlePageChange}
-        onChangeRowsPerPage={handleChangeRowsPerPage}
-      />
+	  <DataSearch placeholder='Search User...' handleFilter={handleFilter} />
+	  {users.length > 0 && (<DataTable
+					columns={columns}
+					data={users}
+					progressPending={isLoading}
+					pagination
+					selectableRows
+					paginationServer
+					fixedHeader
+					customStyles={customStyles}
+					sortIcon={sortIcon}
+					handleButtonClick={handleButtonClick}
+					paginationTotalRows={totalRows}
+					onChangePage={handlePageChange}
+					onChangeRowsPerPage={handleChangeRowsPerPage}
+				/>
+	  )}
     </div>
   );
 };
